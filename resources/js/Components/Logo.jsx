4a89fba1c659e6feaad08dc8098ced753378bcfe
afondevilarevelli <antonny.42@gmail.com>
2023-05-28 03:01:14 -0300

    Logo with border on dark theme

--- resources/js/Components/Logo.jsx
+++ resources/js/Components/Logo.jsx
@@ -1,8 +1,15 @@
+import useAppContext, { DARK_THEME } from "@/contexts/AppContext";
+
 export default function Logo({ size = 100 }) {
+    const { theme } = useAppContext();
+
     return (
         <img
             src="/logo.png"
-            className={"rounded-full"}
+            className={
+                "rounded-full " +
+                (theme == DARK_THEME ? "border-2 border-primary" : "")
+            }
             style={{ width: size, height: size }}
         />
     );
